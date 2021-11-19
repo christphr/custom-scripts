009557f11d6f79c5b0f90fdd8415aedeeba368eb
christphr <77719417+christphr@users.noreply.github.com>
2021-11-19 10:09:40 +0200

    Update NIR_script.js
    Working

--- sentinel-2/ndmi/NIR_script.js
+++ sentinel-2/ndmi/NIR_script.js
@@ -1,11 +1,11 @@
-//NDWI Colourisation from NDMI script
-if (B08 == 0 || B03 == 0){
+//NIR Colourisation from NDMI script
+if (B08 == 0){
   return [0,0,0];
 } else {
   var val = B08;
   
   var vmin = 0;
-  var vmax = 4000;
+  var vmax = 0.3;
   var dv = vmax - vmin;
   
   var r = 0.0;
@@ -22,32 +22,28 @@ if (B08 == 0 || B03 == 0){
     v = vmax;
   }
   
-  var l1 = 0.25;
-  var l2 = 0.499;
-  var l3 = 0.501;
-  var l4 = 0.75;
+  var l1 = 0.333;
+  var l2 = 0.667;
+  //var l3 = 0.5;
+  //var l4 = 0.75;
   
   var level1 = (vmin + l1 * dv);
   var level2 = (vmin + l2 * dv);
-  var level3 = (vmin + l3 * dv);
-  var level4 = (vmin + l4 * dv);
+ // var level3 = (vmin + l3 * dv);
+ // var level4 = (vmin + l4 * dv);
 
   if (v < level1){
-     r = 0.5 +  (v - vmin) / (level1 - vmin) / 2;
-  } else if (v < level2) {
-     r = 1;
-     g = (v - level1) / (level2 - level1);
-     b = 0;
-  } else if (v < level3) {
-     r = 1 + (level2 - v) / (level3 - level2);
-     g = 1;
-     b = (v - level2) / (level3 - level2);
-  } else if (v < level4) {
-     r = 0;
-     g = 1 + (level3 - v) / (level4 - level3);
-     b = 1;
-  } else {
-     b = 1.0 + (level4 - v) / (vmax - level4) / 2;
+    r = 0;
+    g = 0;  
+    b = (v - vmin) / (level1 - vmin);
+  } else if (v < level2){
+    r = (v - level1) / (level2 - level1);
+    g = r;
+    b = (1 - (v - level1) / (level2 - level1));
+  } else { 
+    r = 1;
+    g = 1;
+    b = (v - level2) / (vmax - level2);
   }
 
    return [r, g, b];
